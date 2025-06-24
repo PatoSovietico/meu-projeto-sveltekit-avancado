5a014e1727ed76c3ab6d9f7507e574e233e82abd
PatoSovietico <arthurbat120907@gmail.com>
2025-06-24 12:06:20 -0400

    cu

---
+++ src/routes/06/ifms/a/+page.svelte
@@ -0,0 +1,11 @@
+<form method="POST" action="/06/ifms/a">
+    <input name="umnumero" type="number" placeholder="Digite um número" /><br />
+    <input name="umtexto" type="text" placeholder="Digite um número" /><br />
+    <input name="umadata" type="date" /><br />
+    <button>Enviar 1</button>
+    <button formaction="06/ifms/b">Enviar 2</button>
+    <button formaction="06/ifms/c">Enviar 3</button>
+
+</form>
+
+<a href="/06/ifms/a">Voltar</a>
